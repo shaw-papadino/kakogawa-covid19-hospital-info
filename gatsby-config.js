18e360f813b83ebb5e9326b348f0def0565e29d7
shoyaokayama <sho.edu.human@gmail.com>
2021-06-16 00:39:08 +0900

    analytics追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "kakogawa-covid19-hospital",
   },
-  plugins: [`gatsby-plugin-material-ui`],
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-8RXFHJE1NN", // Google Analytics / GA
+        ],
+      },
+    },
+  ],
   pathPrefix: "/kakogawa-covid19-hospital-info",
 };
